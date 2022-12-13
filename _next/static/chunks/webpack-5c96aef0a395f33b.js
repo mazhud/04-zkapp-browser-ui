2973cf88234ff466d5bfd8b8bf2c199726ca7b85
mazhud <hmen1710@gmail.com>
2022-12-13 02:14:19 +0000

    Deploy gh-pages

--- _next/static/chunks/webpack-5c96aef0a395f33b.js
+++ _next/static/chunks/webpack-5c96aef0a395f33b.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "3a33e19c549c6ae3" + ".css";
+/******/ 			return "static/css/" + "a3a9d0117c30ab94" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
